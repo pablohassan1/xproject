227be2cefbd30f5c1646b2e606c44ee3c0a625e5
Jan Brezina <brez.jan@seznam.cz>
2020-10-11 13:19:49 +0200

    Updates

---
+++ precache-manifest.34db10572933fe4548f32f882cebfdd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bd5e5ede78003d9070e5b49f2267cc",
+    "url": "/xproject/index.html"
+  },
+  {
+    "revision": "f340c50d881e4865a9ef",
+    "url": "/xproject/static/css/main.86a22257.chunk.css"
+  },
+  {
+    "revision": "2d56d573fbbb5e838ad7",
+    "url": "/xproject/static/js/2.bd3411d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xproject/static/js/2.bd3411d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f340c50d881e4865a9ef",
+    "url": "/xproject/static/js/main.d917c0ec.chunk.js"
+  },
+  {
+    "revision": "e2f923ad19f2935b4dc6",
+    "url": "/xproject/static/js/runtime-main.9eb0ec6d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.380fecf610bf5706921d8c10d80822f6.js"
+  "/xproject/precache-manifest.34db10572933fe4548f32f882cebfdd1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/xproject/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
