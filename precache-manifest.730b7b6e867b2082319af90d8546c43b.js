fdf2fe9efc88081dbc33f101b12b1f07458e679c
Jan Brezina <brez.jan@seznam.cz>
2020-10-11 11:09:38 +0200

    Updates

---
+++ precache-manifest.730b7b6e867b2082319af90d8546c43b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69002be57f3747af3b8e63aaa3862b2d",
+    "url": "/xproject/index.html"
+  },
+  {
+    "revision": "70c113ac34e1b6a8ee66",
+    "url": "/xproject/static/css/main.86a22257.chunk.css"
+  },
+  {
+    "revision": "2d56d573fbbb5e838ad7",
+    "url": "/xproject/static/js/2.bd3411d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xproject/static/js/2.bd3411d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c113ac34e1b6a8ee66",
+    "url": "/xproject/static/js/main.5d11480c.chunk.js"
+  },
+  {
+    "revision": "e2f923ad19f2935b4dc6",
+    "url": "/xproject/static/js/runtime-main.9eb0ec6d.js"
+  }
+]);
